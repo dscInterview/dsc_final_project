d96447160c6bf861bd90990b76712f02697722cb
Suezcanaal <sushmalove2003@gmail.com>
2024-12-19 01:51:09 +0530

    ABout us Page Updation

---
+++ client/src/data.js
@@ -0,0 +1,49 @@
+const teamData = [
+    {
+      id: 1,
+      name: "Shantanu Pandey",
+      image: "https://via.placeholder.com/150",
+      skills: "Full Stack Developer, MERN Stack, Machine Learning",
+      linkedin: "https://www.linkedin.com",
+      instagram: "https://www.instagram.com",
+      email: "mailto:shantanupandey@gmail.com",
+    },
+    {
+      id: 2,
+      name: "Hemanth",
+      image: "https://via.placeholder.com/150",
+      skills: "Full Stack Developer, MERN Stack",
+      linkedin: "https://www.linkedin.com",
+      instagram: "https://www.instagram.com",
+      email: "mailto:hemanth@gmail.com",
+    },
+    {
+      id: 3,
+      name: "H G Vasudeva",
+      image: "https://via.placeholder.com/150",
+      skills: "Machine Learning, Data Science",
+      linkedin: "https://www.linkedin.com",
+      instagram: "https://www.instagram.com",
+      email: "mailto:hgvasudeva@gmail.com",
+    },
+    {
+      id: 4,
+      name: "Muskan Kumari",
+      image: "https://via.placeholder.com/150",
+      skills: "UI/UX Designer, Figma, FrontEnd",
+      linkedin: "https://www.linkedin.com",
+      instagram: "https://www.instagram.com",
+      email: "mailto:muskankumari@gmail.com",
+    },
+    {
+        id: 5,
+        name: "Ashwith",
+        image: "https://via.placeholder.com/150",
+        skills: "Full Stack Developer",
+        linkedin: "https://www.linkedin.com",
+        instagram: "https://www.instagram.com",
+        email: "mailto:ashwith@gmail.com",
+      },
+  ];
+  
+  export default teamData;
